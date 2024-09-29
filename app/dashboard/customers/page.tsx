6510069411ba28d941b7e5edab49335b953e3bdd
Michael <mihali5@mail.ru>
2024-09-29 12:00:45 +0300

    final

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  console.log('searchParams', searchParams);
   return <p>Customers Page</p>;
 }
